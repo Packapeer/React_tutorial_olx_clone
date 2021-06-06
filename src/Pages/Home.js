45804c484250cb8c7bd9081c67f37c0582c23739
sidhik64742 <64644392+sidhik4742@users.noreply.github.com>
2021-06-06 21:00:01 +0530

    Update Home.js

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -3,7 +3,6 @@ import React from 'react';
 import Header from '../Components/Header/Header';
 import Banner from '../Components/Banner/Banner';
 
-import './Home.css';
 import Posts from '../Components/Posts/Posts';
 import Footer from '../Components/Footer/Footer';
 
