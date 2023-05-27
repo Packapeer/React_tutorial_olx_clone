f0e101f16a38b22c55f0c082e8fa30339bb6fc16
Muhammed Fariz KP <farizz7676off@gmail.com>
2023-05-27 15:58:24 +0530

    my initial commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,39 @@
-import React from 'react';
+import React,{useEffect,useContext} from 'react';
 import './App.css';
+import { BrowserRouter as Router,Route} from 'react-router-dom';
+import Signup from './Pages/Signup'
+import Login from './Pages/Login';
+import { AuthContext,FirebaseContext } from './store/Context';
 
 /**
  * ?  =====Import Components=====
  */
 import Home from './Pages/Home';
 
+
 function App() {
+  const {setUser}=useContext(AuthContext)
+  const {firebase}=useContext(FirebaseContext)
+  useEffect(()=>{
+    firebase.auth().onAuthStateChanged((user)=>{
+      setUser(user)
+    })
+  })
   return (
     <div>
-      <Home />
+      <Router>
+        <Route exact path='/'> 
+          <Home />
+        </Route>
+
+        <Route path='/signup'> 
+          <Signup />
+        </Route>
+        <Route path='/login'> 
+          <Login />
+        </Route>
+        
+      </Router>
     </div>
   );
 }
